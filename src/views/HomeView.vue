1487dbbcbb0e9e80994584e13bf64b9a15cdfbba
David González <dp@phoenixconnection.es>
2023-03-31 10:54:57 +0200

    Preparar espacion para colocar mapa

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+  <MapView></MapView>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { usePlacesStore } from '@/composables';
+import MapView from '@/components/mapview/MapView.vue';
 
 export default defineComponent({
   name: 'HomeView',
-  components: { },
+  components: { MapView },
   setup() {
       usePlacesStore();
   },
