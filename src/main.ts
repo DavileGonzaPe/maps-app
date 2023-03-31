4fa9d9af697707acbbfa33d0ec399c1b92cd764c
David González <dp@phoenixconnection.es>
2023-03-31 12:33:35 +0200

    Mostrar mapa cuando tenemos la ubicación del usuario

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGF2aWRnb256YWxlenBlcmV6ODEiLCJhIjoiY2xmd2RoamNsMDJ0ZTNjbzk1cTV5bmo5NyJ9.hSZux6zOenlV7TDy4amnaw';
 
 if (!navigator.geolocation) {
     throw new Error('Tu navegador no soporta el GeoLocation')    
