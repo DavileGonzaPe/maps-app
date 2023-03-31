d7752c179d46664ae9f8ef75288c30c2852e4702
David González <dp@phoenixconnection.es>
2023-03-31 10:23:36 +0200

    Composable - usePlacesStore

--- src/store/places/actions.ts
+++ src/store/places/actions.ts
@@ -7,7 +7,7 @@ const actions: ActionTree<PlacesState, StateInterface> = {
     getInitialLocation( { commit } ) {
         // TODO: colocar loading
         navigator.geolocation.getCurrentPosition(
-            (coords) => commit('setLngLat', coords),
+            (coords) => commit('setLngLat', { lng: coords.coords.longitude, lat: coords.coords.latitude }),
             (err) => {
                 console.log(err);
                 throw new Error('No geolocation: ');
