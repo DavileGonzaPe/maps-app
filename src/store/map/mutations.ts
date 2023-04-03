c01adab1709f92fe778f697caf67a37acbac0f54
David González <dp@phoenixconnection.es>
2023-04-03 19:31:01 +0200

    Borrar polyline.

--- src/store/map/mutations.ts
+++ src/store/map/mutations.ts
@@ -33,7 +33,15 @@ const mutation: MutationTree<MapSate> = {
                 
                 state.markers.push( marker );
             }
-        }        
+
+            //Clear polyline
+            if ( state.map.getLayer('RouteString') ) {
+                state.map.removeLayer('RouteString');
+                state.map.removeSource('RouteString');
+                state.distance = undefined;
+                state.duration = undefined;
+            }
+        }
     },
 
     setRoutePolyline( state, coords: number[][] ){
