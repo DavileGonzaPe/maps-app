f23864b6b135e5383620702c44152f392b8ee6e1
David González <dp@phoenixconnection.es>
2023-04-03 17:41:06 +0200

    Activar lugar en el listado y mover mapa al lugar elegido.

--- src/components/search-result/SearchResults.ts
+++ src/components/search-result/SearchResults.ts
@@ -1,14 +1,27 @@
-import { defineComponent } from 'vue';
-import { usePlacesStore } from '@/composables';
+import { defineComponent, ref } from 'vue';
+import { useMapStore, usePlacesStore } from '@/composables';
+import { Feature } from '@/interfaces/places';
 
 export default defineComponent ({
     name: 'SearchResults',
     setup() {
         const { isLoadingPlaces, places } = usePlacesStore();
+        const { map } = useMapStore();
+        const activePlace = ref('');
 
         return {
+            activePlace,
             isLoadingPlaces,
             places,
+
+            onPlaceClicked: ( place: Feature ) => {
+                activePlace.value = place.id;
+                const [ longitude, latitude ] = place.center;
+                map.value?.flyTo({
+                    center: [ longitude, latitude ],
+                    zoom: 14,
+                })
+            }
         }
     }
 });
