8da80a13849bda7609fbb8dbc5ccf2837ec22a40
David González <dp@phoenixconnection.es>
2023-04-03 10:45:20 +0200

    Botón volver a la ubicación del usuario.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
   <MapView></MapView>
+  <MyLocationButton></MyLocationButton>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { usePlacesStore } from '@/composables';
 import MapView from '@/components/mapview/MapView.vue';
+import MyLocationButton from '@/components/my-location-button/MyLocationButton.vue';
 
 export default defineComponent({
   name: 'HomeView',
-  components: { MapView },
+  components: { MapView, MyLocationButton},
   setup() {
       usePlacesStore();
   },
