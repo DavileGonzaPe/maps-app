3742cab1b988ae451c893c1d33d828248eee8d6d
David González <dp@phoenixconnection.es>
2023-04-03 19:25:30 +0200

    Mostar ruta en el mapa.

--- src/store/map/actions.ts
+++ src/store/map/actions.ts
@@ -10,7 +10,7 @@ const actions: ActionTree<MapSate, StateInterface> = {
     async getRouteBetweenPoints( { commit }, { start, end }: { start: LngLat, end: LngLat } ) {
         const resp = await directionsApi.get<DirectionsResponse>(`${ start.join(',') };${ end.join(',') }`);
 
-        console.log(resp.data.routes[0].geometry.coordinates);
+        commit('setRoutePolyline', resp.data.routes[0].geometry.coordinates)
     }
 }
 
