d93c47ffc7510781badbb94e0c371420776a3950
David González <dp@phoenixconnection.es>
2023-04-03 19:38:38 +0200

    Almacenar distancia y duración.

--- src/store/map/actions.ts
+++ src/store/map/actions.ts
@@ -10,6 +10,11 @@ const actions: ActionTree<MapSate, StateInterface> = {
     async getRouteBetweenPoints( { commit }, { start, end }: { start: LngLat, end: LngLat } ) {
         const resp = await directionsApi.get<DirectionsResponse>(`${ start.join(',') };${ end.join(',') }`);
 
+        commit('setDistanceDuration', {
+            distance: resp.data.routes[0].distance,
+            duration: resp.data.routes[0].duration,
+        });
+
         commit('setRoutePolyline', resp.data.routes[0].geometry.coordinates)
     }
 }
