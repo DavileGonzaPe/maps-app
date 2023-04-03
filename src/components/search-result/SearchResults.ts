99f8c92d2e16bb005ee92e76363ed0f7da6e9557
David González <dp@phoenixconnection.es>
2023-04-03 19:01:05 +0200

    Obtener información de la ruta.

--- src/components/search-result/SearchResults.ts
+++ src/components/search-result/SearchResults.ts
@@ -5,8 +5,8 @@ import { Feature } from '@/interfaces/places';
 export default defineComponent ({
     name: 'SearchResults',
     setup() {
-        const { isLoadingPlaces, places } = usePlacesStore();
-        const { map, setPlaceMarkers } = useMapStore();
+        const { isLoadingPlaces, places, userLocation } = usePlacesStore();
+        const { map, setPlaceMarkers, getRouteBetweenPoints } = useMapStore();
         const activePlace = ref('');
 
         watch( places, ( newPlaces ) => {
@@ -21,11 +21,26 @@ export default defineComponent ({
 
             onPlaceClicked: ( place: Feature ) => {
                 activePlace.value = place.id;
+                
                 const [ longitude, latitude ] = place.center;
+
                 map.value?.flyTo({
                     center: [ longitude, latitude ],
                     zoom: 14,
                 })
+            },
+
+            getRouteDirections: ( place: Feature ) => {
+                if (userLocation.value) {
+                    
+                    const [ longitude, latitude ] = place.center;
+                    const [ startLongitude, startLatitude ] = userLocation.value;
+
+                    const start: [number, number] = [ startLongitude, startLatitude ];
+                    const end: [number, number] = [ longitude, latitude ];
+                    
+                    getRouteBetweenPoints( start, end );
+                }
             }
         }
     }
