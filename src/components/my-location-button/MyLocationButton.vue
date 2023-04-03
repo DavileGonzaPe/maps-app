3c2c2f5adfc0b1852abfa7ca0592a23991ef314c
David González <dp@phoenixconnection.es>
2023-04-03 11:15:23 +0200

    SearchResult - Diseño del componente

--- src/components/my-location-button/MyLocationButton.vue
+++ src/components/my-location-button/MyLocationButton.vue
@@ -31,7 +31,7 @@ export default defineComponent({
 });
 </script>
 
-<style scope>
+<style scoped>
 button {
     position: fixed;
     top: 30px;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <MapView></MapView>
+  <SearchBar></SearchBar>
   <MyLocationButton></MyLocationButton>
 </template>
 
@@ -8,10 +9,11 @@ import { defineComponent } from 'vue';
 import { usePlacesStore } from '@/composables';
 import MapView from '@/components/mapview/MapView.vue';
 import MyLocationButton from '@/components/my-location-button/MyLocationButton.vue';
+import SearchBar from '@/components/searchbar/SearchBar.vue';
 
 export default defineComponent({
   name: 'HomeView',
-  components: { MapView, MyLocationButton},
+  components: { MapView, MyLocationButton, SearchBar },
   setup() {
       usePlacesStore();
   },
